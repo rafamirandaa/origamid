4b05ad6ef5acabac430f7cc8ebd4066004173e63
Rafael Miranda <35074501+rafamirandaa@users.noreply.github.com>
2022-08-19 11:03:20 -0300

    desafio resolvido

--- react-origamid/src/App.js
+++ react-origamid/src/App.js
@@ -6,7 +6,7 @@ import Produto from './Produto';
 // https://ranekapi.origamid.dev/json/api/produto/smartphone
 // Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
 // Defina o produto clicado como uma preferência do usuário no localStorage
-// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo.
 
 
 const App = () => {
