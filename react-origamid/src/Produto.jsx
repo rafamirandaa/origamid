9dfb193c78d74de5fee30c5f8107d468d287d485
Rafael Miranda <35074501+rafamirandaa@users.noreply.github.com>
2022-09-05 17:50:57 -0300

    Add foto também

--- react-origamid/src/Produto.jsx
+++ react-origamid/src/Produto.jsx
@@ -5,6 +5,7 @@ export default function Produto({ dados }) {
     <div>
       <h1>{dados.nome}</h1>
       <p>R$ {dados.preco}</p>
+      <p><img src={dados.fotos[0].src} alt="" /></p>
     </div>
   )
 }
